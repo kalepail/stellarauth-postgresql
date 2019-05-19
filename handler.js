a71219dae3516e8d41607411d9cf8108ceb902fe
Tyler van der Hoeven <hi@tyvdh.com>
2019-05-19 15:36:58 -0400

    use the testnet

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { Pool } from 'pg'
 
 const isDev = process.env.NODE_ENV !== 'production'
-const authUrl = isDev ? 'https://localhost:4000/auth' : 'https://api.stellarauth.com/auth'
+const authUrl = isDev ? 'https://localhost:4000/auth' : 'https://api-testnet.stellarauth.com/auth'
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = isDev ? 0 : 1
 
